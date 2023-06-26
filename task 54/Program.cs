41bcf000f52a293f1a37f95357cb59b87ce8c55a
NiKToc <niktoc20@gmail.com>
2023-06-26 19:52:16 +0500

    task 54 and 56

---
+++ task 54/Program.cs
@@ -0,0 +1,49 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 31);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+        
+        Console.WriteLine();
+    }
+}
+
+void SortMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int l = 0; l < matrix.GetLength(1)-1;l++)
+            {
+                if (matrix[i, l] < matrix[i, l + 1])
+                {
+                    int temp = matrix[i, l + 1];
+                    matrix[i, l + 1] = matrix [i, l];
+                    matrix[i, l] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальная матрица: ");
+PrintMatrix(matrix);
+Console.WriteLine("Конечная матрица: ");
+SortMatrix(matrix);
+PrintMatrix(matrix);
---
+++ task 56/Program.cs
@@ -0,0 +1,48 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 101);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]}\t");
+        
+        Console.WriteLine();
+    }
+}
+
+int StringSum (int[,] matrix, int i)
+{
+    int sum = matrix[i, 0];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        sum += matrix[i, j];
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+
+int MinSum = 0;
+int sum = StringSum(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+    int TempSum = StringSum(matrix, i);
+    if (sum > TempSum)
+    {
+        sum = TempSum;
+        MinSum = i;
+    }
+}
+
+Console.WriteLine($"{MinSum + 1} строка");
